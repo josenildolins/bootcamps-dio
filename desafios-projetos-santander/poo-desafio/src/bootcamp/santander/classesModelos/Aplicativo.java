bec3f7f9f3c82563821fdb885d813723430f4786
josenildolins <josenildo.lins@gmail.com>
2023-09-25 14:18:50 -0300

    feat: implementando classe Aplicativo

--- desafios-projetos-santander/poo-desafio/src/bootcamp/santander/classesModelos/Aplicativo.java
+++ desafios-projetos-santander/poo-desafio/src/bootcamp/santander/classesModelos/Aplicativo.java
@@ -1,4 +1,27 @@
 package bootcamp.santander.classesModelos;
 public class Aplicativo {
     
+    private String nome;
+
+    private String logo;
+
+    public Aplicativo(String nome, String logo) {
+        this.nome = nome;
+        this.logo = logo;
+    }
+
+    public Aplicativo() {
+    }
+
+    public void exibirDetalhes(){
+        System.out.println("Exibindo Detalhes");
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
 }
