e8e75d8d79fec54518e7431e0797d0a2a4b3ce7f
josenildolins <josenildo.lins@gmail.com>
2023-09-25 14:20:47 -0300

    feat: implementando classe Ipod

--- desafios-projetos-santander/poo-desafio/src/bootcamp/santander/classesModelos/Ipod.java
+++ desafios-projetos-santander/poo-desafio/src/bootcamp/santander/classesModelos/Ipod.java
@@ -1,4 +1,22 @@
 package bootcamp.santander.classesModelos;
-public class Ipod {
+
+import bootcamp.santander.interfaces.reprodutorMusical;
+
+public class Ipod extends Aplicativo implements reprodutorMusical {
+
+    @Override
+    public void play() {
+        System.out.println("Tocando musica");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Pausando musica");
+    }
+
+    @Override
+    public String selecionarMusica() {
+        return "selecionando musica";
+    }
     
 }
