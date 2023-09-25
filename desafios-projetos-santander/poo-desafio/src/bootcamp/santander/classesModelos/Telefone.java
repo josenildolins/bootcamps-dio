abb050fbe2774d9a12ec747ad5e7e4db73cd1973
josenildolins <josenildo.lins@gmail.com>
2023-09-25 17:04:52 -0300

    feat: inserindo construtor na classe Telefone;

--- desafios-projetos-santander/poo-desafio/src/bootcamp/santander/classesModelos/Telefone.java
+++ desafios-projetos-santander/poo-desafio/src/bootcamp/santander/classesModelos/Telefone.java
@@ -4,6 +4,10 @@ import bootcamp.santander.interfaces.ExecutorDeChamada;
 
 public class Telefone extends Aplicativo  implements ExecutorDeChamada{
 
+    public Telefone(String nome, String logo) {
+        super(nome, logo);
+    }
+    
     @Override
     public void ligar() {
         System.out.println("Ligando...");
