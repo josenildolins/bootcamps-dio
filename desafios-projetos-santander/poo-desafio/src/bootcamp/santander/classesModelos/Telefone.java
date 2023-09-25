273b6cb1419b469dd077daedb0e2f5d26e716cef
josenildolins <josenildo.lins@gmail.com>
2023-09-25 14:21:22 -0300

    feat: Implementando classe Telefone

--- desafios-projetos-santander/poo-desafio/src/bootcamp/santander/classesModelos/Telefone.java
+++ desafios-projetos-santander/poo-desafio/src/bootcamp/santander/classesModelos/Telefone.java
@@ -1,4 +1,22 @@
 package bootcamp.santander.classesModelos;
-public class Telefone {
+
+import bootcamp.santander.interfaces.ExecutorDeChamada;
+
+public class Telefone extends Aplicativo  implements ExecutorDeChamada{
+
+    @Override
+    public void ligar() {
+        System.out.println("Ligando...");
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("atendendo a chamada");
+    }
+
+    @Override
+    public void iniciarCorreioDeVoz() {
+        System.out.println("Chamando correio de voz");
+    }
     
 }
