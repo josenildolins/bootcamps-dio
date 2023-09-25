b34b97f555013e355e3eb03952bdd0b6aaa4d04d
josenildolins <josenildo.lins@gmail.com>
2023-09-25 14:15:09 -0300

    feat: implementando classe Aparelho

--- desafios-projetos-santander/poo-desafio/src/bootcamp/santander/classesModelos/Aparelho.java
+++ desafios-projetos-santander/poo-desafio/src/bootcamp/santander/classesModelos/Aparelho.java
@@ -1,4 +1,36 @@
 package bootcamp.santander.classesModelos;
+
+import java.util.List;
+
 public class Aparelho {
     
+    private String nome;
+
+    private String IMEI;
+
+    private List<Aplicativo> aplicativos;
+
+    
+    public Aparelho(String nome, String iMEI, List<Aplicativo> aplicativos) {
+        this.nome = nome;
+        IMEI = iMEI;
+        this.aplicativos = aplicativos;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getIMEI() {
+        return IMEI;
+    }
+
+    public List<Aplicativo> getAplicativos() {
+        return aplicativos;
+    }
+
+    public void exibiraplicativos(List<Aplicativo> aplicativos){
+        System.out.println("Exibindo Aplicativos");
+    }
+
 }
