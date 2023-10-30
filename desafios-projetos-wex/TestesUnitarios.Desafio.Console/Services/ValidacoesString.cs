1643ee53e3b6b961b6c7d5b7239c27b01e835c75
josenildolins <josenildo.lins@gmail.com>
2023-10-30 11:24:12 -0300

    improvement : substituindo os tipos "var" pelos seus respectivos tipos

--- desafios-projetos-wex/TestesUnitarios.Desafio.Console/Services/ValidacoesString.cs
+++ desafios-projetos-wex/TestesUnitarios.Desafio.Console/Services/ValidacoesString.cs
@@ -4,19 +4,19 @@ namespace TestesUnitarios.Desafio.Console.Services
     {
         public int RetornarQuantidadeCaracteres(string texto)
         {
-            var numeroCaracteres = texto.Length;
+            int numeroCaracteres = texto.Length;
             return numeroCaracteres;
         }
 
         public bool ContemCaractere(string texto, string textoProcurado)
         {
-            var contem = texto.Contains(textoProcurado, StringComparison.OrdinalIgnoreCase);
+            bool contem = texto.Contains(textoProcurado, StringComparison.OrdinalIgnoreCase);
             return contem;
         }
 
         public bool TextoTerminaCom(string texto, string textoProcurado)
         {
-            var termina = texto.EndsWith(textoProcurado, StringComparison.OrdinalIgnoreCase);
+            bool termina = texto.EndsWith(textoProcurado, StringComparison.OrdinalIgnoreCase);
             return termina;
         }
     }
