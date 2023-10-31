2cb1f56b267420d31e5e4ef60e5d6de4be1fc275
josenildolins <josenildo.lins@gmail.com>
2023-10-31 16:25:15 -0300

    feat: implementando os testes

---
+++ desafios-projetos-wex/TDDdesafio/DioBootcampWex/DioBootcampWexTests/CalculadoraServiceTest.cs
@@ -0,0 +1,80 @@
+using DomainService;
+
+namespace DioBootcampWexTests
+{
+    public class CalculadoraServiceTest
+    {
+        private CalculadoraService _calculadora = new CalculadoraService();
+
+        [Fact]
+        public void ShouldReturn4()
+        {
+            //Arrange
+            double x = 2;
+            double y = 2;
+            double expected = 4;
+
+            //Act
+            double result = _calculadora.Somar(x, y);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void ShouldReturn10()
+        {
+            //Arrange
+            double x = 2;
+            double y = 5;
+            double expected = 10;
+
+            //Act
+            double result = _calculadora.Multiplicar(x, y);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void ShouldReturn12()
+        {
+            //Arrange
+            double x = 24;
+            double y = 2;
+            double expected = 12;
+
+            //Act
+            double result = _calculadora.Divide(x, y);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void ShouldReturnDivideByZeroException()
+        {
+            //Arrange
+            double x = 2;
+            double y = 0;
+
+            //Assert
+            Assert.Throws<DivideByZeroException>(() => _calculadora.Divide(x, y));
+        }
+
+        [Fact]
+        public void ShouldReturn5()
+        {
+            //Arrange
+            double x = 10;
+            double y = 5;
+            double expected = 5;
+
+            //Act
+            double result = _calculadora.Subtrair(x, y);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
