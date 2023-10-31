f03992c8e9c891ed2a13536d0698c6e45a1810b7
josenildolins <josenildo.lins@gmail.com>
2023-10-31 17:32:35 -0300

    feat: expondo os metodos no swagger

---
+++ desafios-projetos-wex/TDDdesafio/DioBootcampWex/DioBootcampWex/Controllers/CalculadoraController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DioBootcampWex.Controllers
+{
+    [ApiController]
+    [Route("controller")]
+    public class CalculadoraController: ControllerBase
+    {
+        [HttpPost("somar")]
+        public double somar(double x, double y)
+        {
+            return x + y;
+        }
+
+        [HttpPost("multiplicar")]
+        public double multiplicar(double x, double y)
+        {
+            return 0;
+        }
+
+        [HttpPost("dividir")]
+        public double dividir(double x, double y)
+        {
+            return x / y;
+        }
+
+        [HttpGet("ObterHistorico")]
+        public string ObterHistorico()
+        {
+            return "Histórico";
+        }
+    }
+}
