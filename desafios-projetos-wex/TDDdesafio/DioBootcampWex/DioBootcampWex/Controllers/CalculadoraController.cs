e2fe088c53689068c3ac103cb88cca7e1a0eec6d
josenildolins <josenildo.lins@gmail.com>
2023-10-31 18:41:25 -0300

    feat: Aplicando injecao de dependencia na controller

--- desafios-projetos-wex/TDDdesafio/DioBootcampWex/DioBootcampWex/Controllers/CalculadoraController.cs
+++ desafios-projetos-wex/TDDdesafio/DioBootcampWex/DioBootcampWex/Controllers/CalculadoraController.cs
@@ -1,27 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DomainService.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DioBootcampWex.Controllers
 {
     [ApiController]
     [Route("controller")]
-    public class CalculadoraController: ControllerBase
+    public class CalculadoraController : ControllerBase
     {
+        private readonly IOperacoes _operacoes;
+        public CalculadoraController(IOperacoes operacoes)
+        {
+            _operacoes = operacoes;
+        }
+
         [HttpPost("somar")]
         public double somar(double x, double y)
         {
-            return x + y;
+            return _operacoes.Somar(x, y);
         }
 
         [HttpPost("multiplicar")]
         public double multiplicar(double x, double y)
         {
-            return 0;
+            return _operacoes.Multiplicar(x, y);
         }
 
         [HttpPost("dividir")]
         public double dividir(double x, double y)
         {
-            return x / y;
+            return _operacoes.Divide(x, y);
         }
 
         [HttpGet("ObterHistorico")]
