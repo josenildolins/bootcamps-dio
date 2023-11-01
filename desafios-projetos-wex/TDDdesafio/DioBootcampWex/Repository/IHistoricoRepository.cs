cb0e2f7c96a3fff7aba57a0fd5a43bc685debbe8
josenildolins <josenildo.lins@gmail.com>
2023-11-01 18:15:50 -0300

    feat: criando interface ihistoricorepository

---
+++ desafios-projetos-wex/TDDdesafio/DioBootcampWex/Repository/IHistoricoRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Repository
+{
+    public interface IHistoricoRepository
+    {
+        public void CriarHistorico(double valor1, double valor2, double resultado, DateTime data);
+        public List<Historico> ObterHistorico();
+    }
+}
