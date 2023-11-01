c6273343746bad601c07ba02f519b7c65510cd6f
josenildolins <josenildo.lins@gmail.com>
2023-11-01 18:31:24 -0300

    feat: criando interface icalculadoraservice

---
+++ desafios-projetos-wex/TDDdesafio/DioBootcampWex/DomainService/ICalculadoraService.cs
@@ -0,0 +1,13 @@
+﻿using Repository;
+
+namespace DomainService
+{
+    public interface ICalculadoraService
+    {
+        public double Somar(double x, double y);
+        public double Multiplicar(double x, double y);
+        public double Divide(double x, double y);
+        public double Subtrair(double x, double y);
+        public List<Historico> ObterHistorico();
+    }
+}
