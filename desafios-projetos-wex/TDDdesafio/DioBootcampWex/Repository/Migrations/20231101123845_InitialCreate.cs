c8b1952237d1aa3e6d79608a4653ace73201babe
josenildolins <josenildo.lins@gmail.com>
2023-11-01 18:16:28 -0300

    feat: criando migration

---
+++ desafios-projetos-wex/TDDdesafio/DioBootcampWex/Repository/Migrations/20231101123845_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Historico",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    resultado = table.Column<double>(type: "REAL", nullable: false),
+                    primeiroValor = table.Column<double>(type: "REAL", nullable: false),
+                    segundoValor = table.Column<double>(type: "REAL", nullable: false),
+                    data = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Historico", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Historico");
+        }
+    }
+}
